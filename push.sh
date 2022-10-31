83219f5e0e256517373c1ca22f651d71d3dc29f3
firestaradmin <791836949@qq.com>
2022-10-31 13:18:32 +0800

    Mon Oct 31 13:18:32     2022 with push.sh

--- push.sh
+++ push.sh
@@ -4,7 +4,23 @@ echo "pushing note to github ..."
 echo "=================================="
 git add .
 git status
+
+if [ ! $1 ]; then
+# default opreation
 git commit -m "$(date) with push.sh"
+echo " "
+echo "====================================="
+echo "commit: $(date) with push.sh"
+echo " "
+echo " "
+else
+git commit -m "$1 | with push.sh"
+echo " "
+echo "====================================="
+echo "commit: $1 | with push.sh"
+echo " "
+echo " "
+fi
 git push
 echo " "
 echo " "
